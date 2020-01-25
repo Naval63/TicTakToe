1f0bc28f6d31ca5c2a939d5fef48d5425b387cf4
Bartosz Smalcerz <b.smalcerz@gmail.com>
2020-01-25 21:45:28 +0100

    some functions in js

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,21 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 var PLAYER1 = 'fa-circle-o';
 var PLAYER2 = 'fa-times';
+var round = 1;
+var board = [['', '', ''], ['', '', ''], ['', '', '']];
+var combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+var boxes = _toConsumableArray(document.querySelectorAll('.box'));
+
+boxes.forEach(function (box) {
+  return box.addEventListener('click', pick);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 const PLAYER1 = 'fa-circle-o';
 const PLAYER2 = 'fa-times';
+
+let round = 1;
+const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+const combinations = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+];
+
+const boxes = [...document.querySelectorAll('.box')];
+boxes.forEach(box => box.addEventListener('click', pick));
