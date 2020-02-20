537cc335b9f20db8427c267475a58f5ae7825758
Bartosz Smalcerz <b.smalcerz@gmail.com>
2020-02-20 16:14:30 +0100

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 sass.compiler = require('node-sass');
 const jsSrcPath = 'src/js/**/*.js';
